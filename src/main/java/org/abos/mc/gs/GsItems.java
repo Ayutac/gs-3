d2876653183fcb7278451a228e276809840a600d
ayutac <fly.high.android@gmail.com>
2024-07-15 10:03:50 +0200

    Replaced string with variable.

--- src/main/java/org/abos/mc/gs/GsItems.java
+++ src/main/java/org/abos/mc/gs/GsItems.java
@@ -8,6 +8,6 @@ public interface GsItems {
     // Create a Deferred Register to hold Items which will all be registered under the MODID namespace
     DeferredRegister.Items ITEMS = DeferredRegister.createItems(GnomeSupremacyMod.MODID);
 
-    DeferredItem<BlockItem> INFUSED_STONE = ITEMS.registerSimpleBlockItem("infused_stone", GsBlocks.INFUSED_STONE);
-    DeferredItem<BlockItem> MITHRALIUM_STONE = ITEMS.registerSimpleBlockItem("mithralium_stone", GsBlocks.MITHRALIUM_STONE);
+    DeferredItem<BlockItem> INFUSED_STONE = ITEMS.registerSimpleBlockItem(GsBlocks.INFUSED_STONE.getId().getPath(), GsBlocks.INFUSED_STONE);
+    DeferredItem<BlockItem> MITHRALIUM_STONE = ITEMS.registerSimpleBlockItem(GsBlocks.MITHRALIUM_STONE.getId().getPath(), GsBlocks.MITHRALIUM_STONE);
 }
