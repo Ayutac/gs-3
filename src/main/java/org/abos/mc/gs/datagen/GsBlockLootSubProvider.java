773abe67b582c42a4d8a5aaa717c53b59751d7a0
ayutac <fly.high.android@gmail.com>
2024-07-15 02:12:06 +0200

    Added block loot tables.

---
+++ src/main/java/org/abos/mc/gs/datagen/GsBlockLootSubProvider.java
@@ -0,0 +1,30 @@
+package org.abos.mc.gs.datagen;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.level.block.Block;
+import org.abos.mc.gs.GsBlocks;
+
+import java.util.Set;
+
+public class GsBlockLootSubProvider extends BlockLootSubProvider {
+    protected GsBlockLootSubProvider(HolderLookup.Provider lookupProvider) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), lookupProvider);
+    }
+
+    @Override
+    protected void generate() {
+        dropSelf(GsBlocks.INFUSED_STONE.get());
+        dropSelf(GsBlocks.MITHRALIUM_STONE.get());
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return GsBlocks.BLOCKS.getEntries()
+                .stream()
+                .map(Holder::value)
+                .toList();
+    }
+}
--- src/main/java/org/abos/mc/gs/datagen/GsDatagenHandler.java
+++ src/main/java/org/abos/mc/gs/datagen/GsDatagenHandler.java
@@ -3,12 +3,16 @@ package org.abos.mc.gs.datagen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.abos.mc.gs.GnomeSupremacyMod;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = GnomeSupremacyMod.MODID)
@@ -20,5 +24,8 @@ public class GsDatagenHandler {
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
         generator.addProvider(event.includeClient(), new GsLanguageProvider(output));
+        generator.addProvider(event.includeServer(), new LootTableProvider(output, Set.of(), List.of(
+                new LootTableProvider.SubProviderEntry(GsBlockLootSubProvider::new, LootContextParamSets.BLOCK)
+        ), lookupProvider));
     }
 }
