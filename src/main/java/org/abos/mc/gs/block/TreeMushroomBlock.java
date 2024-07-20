3cd630f9e23c922678f67d7bc8cdb1620ce60f1d
ayutac <fly.high.android@gmail.com>
2024-07-21 01:42:03 +0200

    Tree mushrooms can now only be placed on trees.

--- src/main/java/org/abos/mc/gs/block/TreeMushroomBlock.java
+++ src/main/java/org/abos/mc/gs/block/TreeMushroomBlock.java
@@ -2,9 +2,13 @@ package org.abos.mc.gs.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -39,5 +43,24 @@ public class TreeMushroomBlock extends Block {
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return shapes.getOrDefault(state.getValue(FACING), super.getShape(state, level, pos, context));
+    }
+
+    @Override
+    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return switch (state.getValue(FACING)) {
+            case NORTH -> level.getBlockState(pos.south()).is(BlockTags.LOGS);
+            case EAST -> level.getBlockState(pos.west()).is(BlockTags.LOGS);
+            case SOUTH -> level.getBlockState(pos.north()).is(BlockTags.LOGS);
+            case WEST -> level.getBlockState(pos.east()).is(BlockTags.LOGS);
+            default -> false;
+        };
+    }
+
+    @Override
+    protected BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+        if (!canSurvive(state, level, pos)) {
+            return Blocks.AIR.defaultBlockState();
+        }
+        return super.updateShape(state, direction, neighborState, level, pos, neighborPos);
     }
 }
