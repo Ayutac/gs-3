ec75ed9b5426ebf5a9e18267e0f5b894272875a8
ayutac <fly.high.android@gmail.com>
2024-08-07 09:38:11 +0200

    Removed deprecated resource keys.

--- src/main/java/org/abos/mc/gs/registry/GsConfiguredFeatures.java
+++ src/main/java/org/abos/mc/gs/registry/GsConfiguredFeatures.java
@@ -9,13 +9,9 @@ import org.abos.mc.gs.GnomeSupremacy;
 public interface GsConfiguredFeatures {
 
     ResourceKey<ConfiguredFeature<?, ?>> HUGE_PINK_BONNET = register("huge_pink_bonnet");
-    ResourceKey<ConfiguredFeature<?, ?>> HUGE_PINK_BONNET_PATCH = register("huge_pink_bonnet_patch");
     ResourceKey<ConfiguredFeature<?, ?>> HUGE_LAPIS_DECEIVER = register("huge_lapis_deceiver");
-    ResourceKey<ConfiguredFeature<?, ?>> HUGE_LAPIS_DECEIVER_PATCH = register("huge_lapis_deceiver_patch");
     ResourceKey<ConfiguredFeature<?, ?>> HUGE_MOREL = register("huge_morel");
-    ResourceKey<ConfiguredFeature<?, ?>> HUGE_MOREL_PATCH = register("huge_morel_patch");
     ResourceKey<ConfiguredFeature<?, ?>> HUGE_VIERTOUW_MUSHROOM = register("huge_viertouw_mushroom");
-    ResourceKey<ConfiguredFeature<?, ?>> HUGE_VIERTOUW_MUSHROOM_PATCH = register("huge_viertouw_mushroom_patch");
 
     static ResourceKey<ConfiguredFeature<?, ?>> register(String id) {
         return ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.fromNamespaceAndPath(GnomeSupremacy.MODID, id));
